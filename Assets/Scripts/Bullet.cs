cd2533d42601a7ca8851a7ff3fde192a9b30cd14
Eric <ericpro978@gmail.com>
2025-10-15 18:20:14 +0100

    Mago casi acabado

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,10 +5,14 @@ public class Bullet : MonoBehaviour
 {
     public bool available;
     public Rigidbody2D rigidbody2d;
+    public Vector3 initialPosition;
+    [SerializeField]
+    LayerMask layer;
     // public Rigidbody2D rb => rigidbody2d;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
     {
+        initialPosition = transform.position;
         rigidbody2d = GetComponent<Rigidbody2D>();
         available = true;
 
@@ -27,9 +31,26 @@ public class Bullet : MonoBehaviour
     {
         
     }
-    IEnumerator Die()
+    public IEnumerator Die()
     {
         yield return new WaitForSeconds(5);
         this.gameObject.SetActive(false);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Trigger Enter");
+        if (collision.gameObject.layer == layer)
+        {
+            Debug.Log("Entro al if");
+            this.gameObject.SetActive(false);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision Enter");
+        if (collision.gameObject.layer == layer)
+        {
+            this.gameObject.SetActive(false);
+        }
     }
 }
---
+++ Assets/Scripts/MagoOscuro.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagoOscuro : MonoBehaviour
+{
+    [SerializeField]
+    private Range rangeDetection;
+    [SerializeField]
+    private Range rangeAttack;
+    [SerializeField]
+    private List<GameObject> enemies;
+    [SerializeField]
+    private List<Bullet> ammo;
+    [SerializeField]
+    private List<Transform> pos;
+
+    private void Awake()
+    {
+        rangeAttack.OnEnter += Attack;
+        rangeAttack.OnStay += Attack;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    bool cooldown = false;
+    bool isReloading = false;
+    private void Attack(GameObject enemy)
+    {
+        Debug.Log("Entro a Attack");
+        if (!cooldown)
+        {
+            cooldown = true;
+            Bullet bullet = null;
+            for (int i = 0; i < ammo.Count; i++)
+            {
+                if (ammo[i].available)
+                {
+                    Debug.Log("Entro en bullet y asigno");
+                    bullet = ammo[i];
+                    bullet.available = false;
+                    break;
+                }
+            }
+            if (bullet != null)
+            {
+                bullet.transform.parent = null;
+                Debug.Log("Bullet no es null");
+                Vector2 dir = (enemy.transform.position - bullet.transform.position).normalized;
+                bullet.rigidbody2d.linearVelocity = dir * 6;       
+                bullet.Die();
+                StartCoroutine(ResetCooldown());
+            }
+            else
+            {
+                Debug.Log("Bullet es null");
+                if(AllReload() || !isReloading)
+                    StartCoroutine(Reload());
+            }
+
+        }
+    }
+    IEnumerator ResetCooldown()
+    {
+        yield return new WaitForSeconds(1);
+        cooldown = false;
+    }
+    bool AllReload()
+    {
+        for (int i = 0; i < ammo.Count; i++)
+        {
+            if (!ammo[i].available)
+                return false;
+        }
+        return true;
+    }
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        bool recargado = AllReload();
+
+        while (!recargado)
+        {
+            for (int i = 0; i < ammo.Count; i++)
+            {
+                if (!ammo[i].available)
+                {
+                    yield return new WaitForSeconds(3);
+                    ammo[i].transform.parent = this.transform;
+                    ammo[i].rigidbody2d.linearVelocity = new Vector2(0, 0);
+                    ammo[i].transform.position = pos[i].position;
+                    ammo[i].gameObject.SetActive(true);
+                    ammo[i].available = true;
+                    StartCoroutine(ResetCooldown());
+                }
+            }
+            recargado = AllReload();
+        }
+        StartCoroutine(ResetCooldown());
+        isReloading = false;
+    }
+}
